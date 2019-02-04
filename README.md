90013ac24c5ea6fef201919699dcab8e3d5e7591
Igaguri <igagurimk@gmail.com>
2019-02-04 17:46:41 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# raw-nbt [![raw-nbt at crates.io](https://img.shields.io/crates/v/raw-nbt.svg)](https://crates.io/crates/raw-nbt) [![raw-nbt at docs.rs](https://docs.rs/raw-nbt/badge.svg)](https://docs.rs/raw-nbt)
+
+A low-level NBT(Named Binary Tag) parser for Rust.
+
+## License
+
+Licensed under either of
+
+ * Apache License, Version 2.0
+   ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license
+   ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+## Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
---
+++ tests/loadtest.rs
@@ -0,0 +1,68 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufReader, Read};
+
+use flate2::read::GzDecoder;
+
+use raw_nbt::Parser;
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+#[test]
+fn load_idcounts() {
+    match wrapped_load("./testdata/idcounts.dat", false) {
+        Ok(_) => {}
+        Err(e) => panic!(e.to_string()),
+    }
+}
+
+#[test]
+fn load_level() {
+    match wrapped_load("./testdata/level.dat", true) {
+        Ok(_) => {}
+        Err(e) => panic!(e.to_string()),
+    }
+}
+
+#[test]
+fn load_map() {
+    match wrapped_load("./testdata/map_9.dat", true) {
+        Ok(_) => {}
+        Err(e) => panic!(e.to_string()),
+    }
+}
+
+#[test]
+fn load_villages() {
+    match wrapped_load("./testdata/villages.dat", true) {
+        Ok(_) => {}
+        Err(e) => panic!(e.to_string()),
+    }
+}
+
+//// test util ////
+
+fn wrapped_load(path: &str, gzipped: bool) -> Result<()> {
+    let f = File::open(path)?;
+
+    if gzipped {
+        let r = GzDecoder::new(f);
+        assert_parse(r)
+    } else {
+        let r = BufReader::new(f);
+        assert_parse(r)
+    }
+}
+
+fn assert_parse<R: Read>(r: R) -> Result<()> {
+    let mut parser = Parser::new(r);
+
+    match parser.parse() {
+        Ok(nbt) => {
+            eprintln!("{:?}", nbt);
+        }
+        Err(e) => return Err(Box::new(e)),
+    }
+
+    Ok(())
+}
